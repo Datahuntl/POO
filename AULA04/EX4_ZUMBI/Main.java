34657cf1f34425b714ef31f514a5b3654897ccf3
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-08-12 20:48:55 -0300

    Create Main.java

---
+++ AULA04/EX4_ZUMBI/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Zumbi Z1 = new Zumbi();
+        Zumbi Z2 = new Zumbi();
+
+        Z1.nome = "Jobesvaldo";
+        Z1.vida = 25;
+
+        Z2.nome = "Klóvis";
+        Z2.vida = 30;
+
+        System.out.println(Z1.mostraVida());
+        System.out.println(Z2.mostraVida());
+
+        System.out.println(Z1.transfereVida(Z2, 40));
+        System.out.println(Z1.mostraVida());
+        System.out.println(Z2.mostraVida());
+
+        System.out.println(Z1.transfereVida(Z2, 5));
+        System.out.println(Z1.mostraVida());
+        System.out.println(Z2.mostraVida());
+    }
+}
