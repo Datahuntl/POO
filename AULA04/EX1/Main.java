2bb6e1dbbb52a2e712ae85c76d4823dcc026d6b1
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-08-12 19:54:58 -0300

    Create Main.java

---
+++ AULA04/EX1/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Zumbi Z1 = new Zumbi();
+
+        Z1.nome = "Jobesvaldo";
+        Z1.vida = 23.5;
+        Z1.forca = 6;
+
+        Z1.Info();
+        Z1.Atacar();
+        Z1.Comer();
+    }
+}
