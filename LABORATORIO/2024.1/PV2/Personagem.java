7d444ed36ebe3d2d864e23f0903ecfaee7831aa2
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-08-14 15:39:26 -0300

    Create Personagem.java

---
+++ LABORATORIO/2024.1/PV2/Personagem.java
@@ -0,0 +1,13 @@
+public abstract class Personagem {
+
+    String nome;
+    int vida;
+    int energia;
+    int poder;
+
+    public void usarHabilidade()
+    {
+        System.out.println("Personagem não tem habilidade...");
+    }
+
+}
