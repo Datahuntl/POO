b5a583c57541360e2fdaae1f495af0eb6c2e09fc
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-08-19 21:00:22 -0300

    Create Motor.java

---
+++ AULA04/EX6_KART/Motor.java
@@ -0,0 +1,11 @@
+public class Motor {
+    String cilindrada;
+    float velocidadeMaxima;
+
+    public void mostraInfo()
+    {
+        System.out.println("Mostrando informações do motor:");
+        System.out.println("Velocidade máxima: " + velocidadeMaxima);
+        System.out.println("Cilindrada: " + cilindrada);
+    }
+}
