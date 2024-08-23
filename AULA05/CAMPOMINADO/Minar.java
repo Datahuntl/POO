9d46c994df8d9c181b51886b8b36f2d974a25da9
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-08-22 22:44:15 -0300

    Create Minar.java

---
+++ AULA05/CAMPOMINADO/Minar.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class Minar {
+    Random rand = new Random();
+    int x = rand.nextInt(6);
+    int y = rand.nextInt(6);
+
+    public void Minar()
+    {
+
+    }
+}
