9815b1cc8c42508baf3f8e87f9f3245dd3210500
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-09-16 20:41:07 -0300

    Create Bibliotecario.java

---
+++ LABORATORIO/ATIVIDADE1/Bibliotecario.java
@@ -0,0 +1,46 @@
+public class Bibliotecario {
+    private String nome;
+    private int idade;
+    private boolean disponivel;
+
+    public void listarLivros(Biblioteca biblioteca)
+    {
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+
+    public String getNome()
+    {
+        return this.nome;
+    }
+
+    public void setIdade(int idade)
+    {
+        this.idade = idade;
+    }
+
+    public int getIdade()
+    {
+        return this.idade;
+    }
+
+    public void setDisponivel(boolean disponivel)
+    {
+        this.disponivel = disponivel;
+    }
+
+    public boolean getDisponivel()
+    {
+        return disponivel;
+    }
+
+}
