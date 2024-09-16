92b3b200b98cc9e933ba350036ef0401a5daef21
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-09-16 20:41:21 -0300

    Create Livro.java

---
+++ LABORATORIO/ATIVIDADE1/Livro.java
@@ -0,0 +1,28 @@
+public class Livro {
+    private String titulo;
+    private int nPaginas;
+    private String categoria;
+    private String nomeAutor;
+
+    public Livro(String titulo, int nPaginas, String categoria)
+    {
+        this.titulo = titulo;
+        this.nPaginas = nPaginas;
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public void setNomeAutor(String nomeAutor)
+    {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public String getNomeAutor()
+    {
+        return this.nomeAutor;
+    }
+}
