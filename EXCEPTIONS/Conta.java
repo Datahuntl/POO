2af0f32582dac463a5b15bda5bb7aaf6fa0fca15
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-10-31 22:13:56 -0300

    Create Conta.java

---
+++ EXCEPTIONS/Conta.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Conta {
+
+    private float saldo;
+    private float limite;
+    private Set<Cliente> clientes = new HashSet<>();
+
+
+    public Conta(float saldo, float limite) {
+        this.saldo = saldo;
+        this.limite = limite;
+    }
+
+    public void addCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    public void mostraInfo() {
+        System.out.println("Saldo: " + saldo);
+        System.out.println("Limite: " + limite);
+
+
+        for (Cliente cliente : clientes) {
+            try {
+                System.out.println("Nome do cliente: " + cliente.getNome());
+            } catch (NullPointerException e) {
+                System.out.println("Nao existe cliente nessa posicao");
+            }
+        }
+    }
+
+
+
+
+}
