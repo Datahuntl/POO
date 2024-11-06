de5842c7e0fa9266c1506a7040289e639bd60035
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-11-06 08:55:46 -0300

    Create Temperatura1.java

---
+++ Threads/LAB/EX/Temperatura1.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Temperatura1 implements Runnable{
+
+    Random rand = new Random();
+    int rand_temp = rand.nextInt(30,40);
+
+    @Override
+    public void run()
+    {
+        do
+        {
+            rand_temp = rand.nextInt(30,41);
+            System.out.println("TEMP1: " + rand_temp);
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+        while(rand_temp != 40);
+
+    }
+}
