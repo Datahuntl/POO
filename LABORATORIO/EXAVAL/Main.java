9a256559eca303964080b86003458f5325779d6a
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-11-07 22:19:35 -0300

    Create Main.java

---
+++ LABORATORIO/EXAVAL/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Aluno A1 = new Aluno("Humberto", 21, 3412);
+        Aluno A2 = new Aluno("Yago", 19, 1239);
+        Aluno A3 = new Aluno("Frota", 18, 930);
+
+        Professor P = new Professor("Rocha", 56, 290);
+
+
+
+    }
+}
