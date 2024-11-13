02336cb2f3f2351a305ffcd7b39e1e59e075f92e
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-11-13 07:39:00 -0300

    Update Main.java

--- LABORATORIO/EXAVAL/Main.java
+++ LABORATORIO/EXAVAL/Main.java
@@ -1,13 +1,51 @@
 public class Main {
     public static void main(String[] args) {
 
-        Aluno A1 = new Aluno("Humberto", 21, 3412);
-        Aluno A2 = new Aluno("Yago", 19, 1239);
-        Aluno A3 = new Aluno("Frota", 18, 930);
+        Aluno a1 = new Aluno("Jonas", 20, 344);
+        Aluno a2 = new Aluno("Chris Lima", 19, 555);
+        Aluno a3 = new Aluno("Manoel Gomes", 50, 1000);
 
-        Professor P = new Professor("Rocha", 56, 290);
+        Professor p1 = new Professor("Soned", 50, 30);
+
+        p1.addAlunoLista(a1);
+        p1.addAlunoLista(a2);
+        p1.addAlunoLista(a3);
 
 
+        a1.fazerProva(50);
+        a1.fazerProva(80);
+        a1.fazerProva(100);
 
+        a2.fazerProva(30);
+        a2.fazerProva(0);
+        a2.fazerProva(20);
+
+        a3.fazerProva(2);
+        a3.fazerProva(100);
+        a3.fazerProva(80);
+
+        try {
+            a1.acessarSistema();
+        }catch (NotaInsuficienteException e)
+        {
+            e.printStackTrace();
+        }
+
+        try {
+            a2.acessarSistema();
+        }catch(NotaInsuficienteException e)
+        {
+            e.printStackTrace();
+            //Tambem podia fazer system out (e.getMessage());
+        }
+
+        try{
+            a3.acessarSistema();
+        }catch (NotaInsuficienteException e)
+        {
+            e.printStackTrace();
+        }
+
+        p1.acessarSistema();
     }
 }
