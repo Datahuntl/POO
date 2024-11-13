2b91225092860f0aa175c1f1a7835d58f8375e37
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-11-13 07:39:56 -0300

    Update Usuario.java

--- LABORATORIO/EXAVAL/Usuario.java
+++ LABORATORIO/EXAVAL/Usuario.java
@@ -1,10 +1,10 @@
-public abstract class Usuario {
+public abstract class Usuario implements Autenticavel {
     protected String nome;
     protected int idade;
     protected int matricula;
 
-    public Usuario(String nome, int idade, int matricula)
-    {
+
+    public Usuario(String nome, int idade, int matricula) {
         this.nome = nome;
         this.idade = idade;
         this.matricula = matricula;
