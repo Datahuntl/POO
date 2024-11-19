e8e836140d336d8e9fadf1823ff7d5c3ac32848b
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-11-18 23:10:29 -0300

    Create Instagram.java

---
+++ RedesSociais/Instagram.java
@@ -0,0 +1,22 @@
+public class Instagram extends RedeSocial implements Compartilhamento {
+
+    @Override
+    public void Compartilhar() {
+        System.out.println("Compartilhando no Instagram");
+    }
+
+    @Override
+    public void postarFoto() {
+        System.out.println("Postando Foto no Instagram");
+    }
+
+    @Override
+    public void postarVideo() {
+        System.out.println("Postando Video no Instagram");
+    }
+
+    @Override
+    public void postarComentario() {
+        System.out.println("Postando Comentario no Instagram");
+    }
+}
