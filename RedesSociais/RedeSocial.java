d2fe19d7f8ab8968a8964306f8fe1868174fb09b
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-11-18 23:12:44 -0300

    Create RedeSocial.java

---
+++ RedesSociais/RedeSocial.java
@@ -0,0 +1,17 @@
+package RedesSociais.Execeptions;
+
+public abstract class RedeSocial {
+
+    protected String senha;
+    protected int numAmigos;
+
+    public abstract void postarFoto();
+
+    public abstract void postarVideo();
+
+    public abstract void postarComentario();
+
+    public void curtirPublicacao() {
+        System.out.println("Curtindo Publicação...");
+    }
+}
